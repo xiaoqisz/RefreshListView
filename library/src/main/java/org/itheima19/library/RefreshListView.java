cb7bdce3d079a61b08df00d89129e8c1a8ba2fee
wangdake <xiaoqizcf@gmail.com>
2016-02-20 15:50:23 +0800

    下拉刷新简单拖动

--- library/src/main/java/org/itheima19/library/RefreshListView.java
+++ library/src/main/java/org/itheima19/library/RefreshListView.java
@@ -3,6 +3,7 @@ package org.itheima19.library;
 import android.content.Context;
 import android.util.AttributeSet;
 import android.util.Log;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.ListView;
 
@@ -18,6 +19,9 @@ public class RefreshListView
         extends ListView
 {
     private static final String TAG = "RefreshListView";
+    private float mDownY;
+    private int   mRefreshHeight;
+    private View  mRefreshHeader;
 
     public RefreshListView(Context context) {
         this(context, null);
@@ -32,20 +36,54 @@ public class RefreshListView
 
     private void initRfreshHeader() {
         //添加头
-        View refreshHeader = View.inflate(getContext(), R.layout.refresh_header, null);
-        this.addHeaderView(refreshHeader);
-
+        mRefreshHeader = View.inflate(getContext(), R.layout.refresh_header, null);
+        this.addHeaderView(mRefreshHeader);
 
         //measure(width,height) ---> layout ---> draw
 
         //希望看不到刷新的头,如果用户手势拖动时才可以看到
         //隐藏头
-        refreshHeader.measure(0, 0);
-        int refreshHeight = refreshHeader.getMeasuredHeight();
-        Log.d(TAG, "refreshHeight : " + refreshHeight);
-        int top = -refreshHeight;
-        refreshHeader.setPadding(0, top, 0, 0);
+        mRefreshHeader.measure(0, 0);
+        mRefreshHeight = mRefreshHeader.getMeasuredHeight();
+        Log.d(TAG, "refreshHeight : " + mRefreshHeight);
+        int top = -mRefreshHeight;
+        mRefreshHeader.setPadding(0, top, 0, 0);
     }
 
+    @Override
+    public boolean onTouchEvent(MotionEvent ev) {
 
+
+        switch (ev.getAction()) {
+            case MotionEvent.ACTION_DOWN:
+                mDownY = ev.getY();
+
+                break;
+            case MotionEvent.ACTION_MOVE:
+                // 拖动时
+
+                float moveY = ev.getY();
+
+                float diffY = moveY - mDownY;
+
+
+                //当第0个可见时，用户是由上往下拉动时(diffY > 0)，需要刷新头可见
+                int firstVisiblePosition = this.getFirstVisiblePosition();
+                if (diffY > 0 && firstVisiblePosition == 0) {
+                    // 需要刷新头可见
+                    int top = (int) (diffY - mRefreshHeight + 0.5f);
+                    mRefreshHeader.setPadding(0, top, 0, 0);
+
+                    Log.d(TAG,"设置paddingTop的值 : " + top);
+
+                }
+
+                break;
+            case MotionEvent.ACTION_UP:
+                break;
+        }
+
+
+        return super.onTouchEvent(ev);
+    }
 }

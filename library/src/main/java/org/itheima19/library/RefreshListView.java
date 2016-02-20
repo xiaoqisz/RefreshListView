1b9e546182770b34264a9d5a7481fcad38c85e5b
wangdake <xiaoqizcf@gmail.com>
2016-02-20 14:50:35 +0800

    init

---
+++ library/src/main/java/org/itheima19/library/RefreshListView.java
@@ -0,0 +1,23 @@
+package org.itheima19.library;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.ListView;
+
+/*
+ *  @项目名：  RefreshListView 
+ *  @包名：    org.itheima19.library
+ *  @文件名:   RefreshListView
+ *  @创建者:   Administrator
+ *  @创建时间:  2016/2/20 14:35
+ *  @描述：    TODO
+ */
+public class RefreshListView extends ListView {
+    public RefreshListView(Context context) {
+        super(context);
+    }
+
+    public RefreshListView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+}
--- sample/src/main/java/org/itheima19/sample/MainActivity.java
+++ sample/src/main/java/org/itheima19/sample/MainActivity.java
@@ -1,55 +1,88 @@
 package org.itheima19.sample;
 
+import android.app.Activity;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.TextView;
+
+import org.itheima19.library.RefreshListView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity
-        extends AppCompatActivity
+        extends Activity
 {
+    private RefreshListView mListView;
+
+    private List<String> mDatas;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+        setContentView(R.layout.content_main);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null)
-                        .show();
-            }
-        });
-    }
+        mListView = (RefreshListView) findViewById(R.id.listview);
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        mDatas = new ArrayList<>();
+        for (int i = 0; i < 20; i++) {
+            mDatas.add("数据-" + i);
         }
 
-        return super.onOptionsItemSelected(item);
+        mListView.setAdapter(new RefreshAdapter());// -->list
     }
+
+    private class RefreshAdapter extends BaseAdapter {
+
+        @Override
+        public int getCount() {
+            if (mDatas != null) {
+                return mDatas.size();
+            }
+            return 0;
+        }
+
+        @Override
+        public Object getItem(int position) {
+            if (mDatas != null) {
+                return mDatas.get(position);
+            }
+            return null;
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return position;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            ViewHolder holder = null;
+
+            if (convertView == null) {
+                convertView = View.inflate(MainActivity.this,R.layout.item,null);
+                holder = new ViewHolder();
+                convertView.setTag(holder);
+                holder.tv = (TextView) convertView.findViewById(R.id.item_tv);
+            } else {
+                holder = (ViewHolder) convertView.getTag();
+            }
+
+            String data = mDatas.get(position);
+
+            holder.tv.setText(data);
+
+
+            return convertView;
+        }
+    }
+
+    private class ViewHolder {
+        TextView tv;
+    }
+
+
 }

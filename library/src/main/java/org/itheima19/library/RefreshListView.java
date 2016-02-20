ee80bf9df472afe9fddfc83400c5f4a77ef18f38
wangdake <xiaoqizcf@gmail.com>
2016-02-20 16:03:10 +0800

    下拉刷新状态的分析

--- library/src/main/java/org/itheima19/library/RefreshListView.java
+++ library/src/main/java/org/itheima19/library/RefreshListView.java
@@ -18,10 +18,15 @@ import android.widget.ListView;
 public class RefreshListView
         extends ListView
 {
-    private static final String TAG = "RefreshListView";
+    private static final String TAG                   = "RefreshListView";
+    private static final int    STATE_PULL_DOWN       = 0;
+    private static final int    STATE_RELEASE_REFRESH = 1;
+    private static final int    STATE_REFRESHING      = 2;
     private float mDownY;
     private int   mRefreshHeight;
     private View  mRefreshHeader;
+
+    private int mCurrentState = STATE_PULL_DOWN;//记录刷新的状态
 
     public RefreshListView(Context context) {
         this(context, null);
@@ -74,12 +79,31 @@ public class RefreshListView
                     int top = (int) (diffY - mRefreshHeight + 0.5f);
                     mRefreshHeader.setPadding(0, top, 0, 0);
 
-                    Log.d(TAG,"设置paddingTop的值 : " + top);
+                    //                    Log.d(TAG, "设置paddingTop的值 : " + top);
+
+                    //当用户拉到到某个临界点时，显示为释放刷新
+                    if (top >= 0 && mCurrentState != STATE_RELEASE_REFRESH) {
+                        //如果不是释放刷新的状态时才可以变为释放刷新
+                        // 显示为释放刷新
+                        Log.d(TAG,"显示为释放刷新");
+                        mCurrentState = STATE_RELEASE_REFRESH;
+                        // 根据状态改变 UI TODO:
+
+                    } else if(top < 0 && mCurrentState != STATE_PULL_DOWN) {
+                        //当用户没有超过某个临界点时，显示为下拉刷新
+                        Log.d(TAG,"显示为下拉刷新");
+                        mCurrentState = STATE_PULL_DOWN;
+
+                        // 根据状态改变 UI TODO:
+                    }
+
 
                 }
 
                 break;
             case MotionEvent.ACTION_UP:
+
+                // TODO: 松开时正在刷新
                 break;
         }
 

f30ccf2b5c98fa6281dc916f8fd09bf974d77633
wangdake <xiaoqizcf@gmail.com>
2016-02-20 15:37:31 +0800

    下拉刷新自定义头的添加

--- library/src/main/java/org/itheima19/library/RefreshListView.java
+++ library/src/main/java/org/itheima19/library/RefreshListView.java
@@ -2,6 +2,8 @@ package org.itheima19.library;
 
 import android.content.Context;
 import android.util.AttributeSet;
+import android.util.Log;
+import android.view.View;
 import android.widget.ListView;
 
 /*
@@ -12,12 +14,38 @@ import android.widget.ListView;
  *  @创建时间:  2016/2/20 14:35
  *  @描述：    TODO
  */
-public class RefreshListView extends ListView {
+public class RefreshListView
+        extends ListView
+{
+    private static final String TAG = "RefreshListView";
+
     public RefreshListView(Context context) {
-        super(context);
+        this(context, null);
     }
 
     public RefreshListView(Context context, AttributeSet attrs) {
         super(context, attrs);
+
+        //初始化刷新的头
+        initRfreshHeader();
     }
+
+    private void initRfreshHeader() {
+        //添加头
+        View refreshHeader = View.inflate(getContext(), R.layout.refresh_header, null);
+        this.addHeaderView(refreshHeader);
+
+
+        //measure(width,height) ---> layout ---> draw
+
+        //希望看不到刷新的头,如果用户手势拖动时才可以看到
+        //隐藏头
+        refreshHeader.measure(0, 0);
+        int refreshHeight = refreshHeader.getMeasuredHeight();
+        Log.d(TAG, "refreshHeight : " + refreshHeight);
+        int top = -refreshHeight;
+        refreshHeader.setPadding(0, top, 0, 0);
+    }
+
+
 }
